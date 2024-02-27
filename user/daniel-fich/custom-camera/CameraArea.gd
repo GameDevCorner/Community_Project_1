11c70407c2130e54b3fafc125ae4c76f7ecee5db
Daniel Fich <danielfich2@gmail.com>
2024-02-27 14:40:09 +0100

    Camera area target

--- user/daniel-fich/custom-camera/CameraArea.gd
+++ user/daniel-fich/custom-camera/CameraArea.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @export var area_offset : Vector2
+@export var target : Node
 
-signal camera_area_entered(new_offset: Vector2)
+signal camera_area_entered(new_offset: Vector2, target: Node)
 
 
 func _ready():
@@ -13,7 +14,7 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
-		camera_area_entered.emit(area_offset)
+		camera_area_entered.emit(area_offset, target)
 
 
 func _on_body_exited(body):
--- user/daniel-fich/custom-camera/CustomCamera2D.gd
+++ user/daniel-fich/custom-camera/CustomCamera2D.gd
@@ -2,13 +2,17 @@ extends Camera2D
 
 @export var player : Node
 
+var target_node : Node
 var target : Vector2
 var target_offset : Vector2
 var target_offset_target : Vector2
 
 func _ready():
-	position = player.position 
-	target = player.position
+	#position = player.position 
+	#target = player.position
+	target_node = player
+	target = target_node.position
+	position = target
 	target_offset = Vector2(0, 0)
 	target_offset_target = Vector2(0, 0)
 	
@@ -18,11 +22,18 @@ func disable_main_camera():
 	player.get_node("Camera2D").enabled = false
 
 func _process(delta):
-	target = player.position + target_offset
+	target = target_node.position + target_offset
 	target_offset = target_offset.lerp(target_offset_target, delta * 5)
 	position = position.lerp(target, delta * 5)
 
 
-func set_camera_offset(new_offset = Vector2.ZERO):
+func set_camera_offset(new_offset = Vector2.ZERO, new_target: Node = null):
 	target_offset_target = new_offset
+	print(new_target)
+	if new_target == null:
+		target_node = player
+	else:
+		target_node = new_target
+	
+	#target_node = player
 
